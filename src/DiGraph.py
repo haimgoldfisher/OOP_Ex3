78c27532c497cc2ba185b21270b2906959e99f67
HaimGoldfisher <haimngoldfisher@gmail.com>
2021-12-20 18:17:30 +0200

    implementation interface

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,4 +1,7 @@
-import GraphInterface
+from GraphInterface import GraphInterface
+from Loc_Node_Edge import Location
+from Loc_Node_Edge import Node
+from Loc_Node_Edge import Edge
 
 
 class DiGraph(GraphInterface):
@@ -6,3 +9,55 @@ class DiGraph(GraphInterface):
         self.key_nodes = key_nodes
         self.key_edges = key_edges
         self.mc = mc
+
+    def v_size(self) -> int:
+        return len(self.key_nodes)
+
+    def e_size(self) -> int:
+        return len(self.key_edges)
+
+    def get_all_v(self) -> dict:
+        return self.key_nodes
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        pass
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        return self.key_nodes.get(id1).edges_to_children
+
+    def get_mc(self) -> int:
+        return self.mc
+
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        pass
+
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        pass
+
+    def remove_node(self, node_id: int) -> bool:
+        pass
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        pass
+
+    def my_dfs(self, start: int) -> int:
+        counter = 0
+        key_visited = dict
+        keys = dict
+        keys.update(self.key_nodes.keys())
+        for key in keys:
+            key_visited.update(key, False)
+        stack = [start]
+        key_visited.update(start, True)
+        counter = counter + 1
+        while len(stack) is not 0:
+            curr_id = stack.pop()
+            curr_node = self.key_nodes.get(curr_id)
+            for child_key in curr_node.children_keys:
+                if key_visited.get(child_key) is not None and key_visited.get(child_key) is False:
+                    stack.append(child_key)
+                    key_visited.update(child_key, True)
+                    counter = counter + 1
+        return counter
+
+
--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,7 +1,33 @@
-import GraphAlgoInterface
-import DiGraph
+from typing import List
+import json
+import GraphInterface
+from GraphAlgoInterface import GraphAlgoInterface
+from DiGraph import DiGraph
+from Loc_Node_Edge import Node
+from Loc_Node_Edge import Edge
 
 
 class GraphAlgo(GraphAlgoInterface):
     def __init__(self, graph: DiGraph) -> None:
         self.graph = graph
+
+    def get_graph(self) -> GraphInterface:
+        return self.graph
+
+    def load_from_json(self, file_name: str) -> bool:
+        pass
+
+    def save_to_json(self, file_name: str) -> bool:
+        pass
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        pass
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        pass
+
+    def centerPoint(self) -> (int, float):
+        pass
+
+    def plot_graph(self) -> None:
+        pass
